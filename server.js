4119533c7a61064f6baad4677bb738d2abc7bc9d
nikitagarg <niki12garg@gmail.com>
2017-03-04 16:25:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,10 +73,11 @@ function createTemplete(data) {
             <head>
                 <title> ${title} </title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
             </head>
             
             <body>
-                <div class = "containerr">
+                <div class = "container">
                     <div>
                         <a href='/'>Home</a>
                     </div>

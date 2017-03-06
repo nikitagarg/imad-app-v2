42536688a7a045c4fefff019ff9dd3d38b757a9a
nikitagarg <niki12garg@gmail.com>
2017-03-06 20:48:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,11 @@
-console.log('Loaded!');
-// change the value of index pge
+//Counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value";
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft= marginLeft + 'px';
-}
-
-img.onclick = function(){
+button.onClick = function(){
     
-    var interval = setInterval(moveRight,50);
-    //img.style.marginLeft ='100px';
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
 }

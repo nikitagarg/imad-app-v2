de0f420cb89c1fb088487407f2e7c1404b77c217
nikitagarg <niki12garg@gmail.com>
2017-03-07 00:08:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,21 @@
 //Counter code
 console.log("hghhj");
 var button = document.getElementById("counter");
-var counter = 0;
+
 button.onclick = function(){
     
     var request = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+        //else do nothing
+    }
     
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    request.open("GET",'http://nikitagarg.imad.hasura-app.io/counter', true);
+    request.send(null);
 };

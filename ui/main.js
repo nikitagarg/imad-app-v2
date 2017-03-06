0edee9049264d5095b336f82dd6b1709a2764386
nikitagarg <niki12garg@gmail.com>
2017-03-06 23:56:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,9 @@ console.log("hghhj");
 var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
     counter = counter + 1;
     var span = document.getElementById("count");
     span.innerHTML = counter.toString();

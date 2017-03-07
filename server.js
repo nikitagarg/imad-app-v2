0a573e9648977899a35fc2095c5034f7279fde39
nikitagarg <niki12garg@gmail.com>
2017-03-08 00:34:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,11 +126,11 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function(req,res){
-    var name= req.query.names;
+app.get('/submit-name/:name', function(req,res){
+    var name= req.params.names;
     names.push(name);
-    
-    res.send(JSON.stringify(names));
+    res.send(names);
+    //res.send(JSON.stringify(names));
     
 });
 

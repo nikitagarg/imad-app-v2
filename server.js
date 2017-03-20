871a854b5c97bbe91b9f4c32915c182913c14885
nikitagarg <niki12garg@gmail.com>
2017-03-20 19:11:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
+
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'nikitagarg',
+  password: process.env.DB_PASSWORD,
+  database: 'nikitagarg',
+  port: '5432',
+};
+
 
 var articles = {
     'article-one': {
@@ -93,7 +102,18 @@ function createTemplete(data) {
     `;
     return htmlTemplete;
 }
-
+var pool = new Pool(config)
+app.get('/test-db', function (req, res){
+   //make a select request
+   //return the result
+   pool.query("SELECT * FROM test", function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result));
+       }
+   });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));

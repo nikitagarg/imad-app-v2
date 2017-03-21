188118d3f01aa8a90a20a61613e941d641773629
nikitagarg <niki12garg@gmail.com>
2017-03-21 12:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,11 +134,26 @@ app.get('/submit-name', function(req,res){
     
 });
 
-app.get('/:articleName',function(req,res) {
+app.get('articles/:articleName',function(req,res) {
     //articleName== article-one
     //articles[articleName]=={}content object for article one
-    var articleName= req.params.articleName;
-    res.send(createTemplete(articles[articleName]));
+    //var articleName= req.params.articleName;
+    //"* FROM article WHERE title = 'article-one'"
+    pool.query("SELECT * FROM article WHERE title = '" + req.param.articleName + "'", function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length === 0){
+                res.status(404).send("Article not found");
+            }
+            else{
+                var articleData = result.rows[0];
+                res.send(createTemplete(articleData));
+            }
+        }
+    });
+    
 });
 
 

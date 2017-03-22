b014742043f466a6ea344394cb5a5bab25526f30
nikitagarg <niki12garg@gmail.com>
2017-03-22 17:13:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/hash/:input', function(req, res){
     res.send(hashedString);
 });
 
-app.post('/create-user', function(req, res){
+app.get('/create-user', function(req, res){
     
     var username = req.body.username;
     var password = req.body.password;

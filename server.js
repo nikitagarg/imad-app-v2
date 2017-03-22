122020ffe9d328500b290d9586ed5897291e6016
nikitagarg <niki12garg@gmail.com>
2017-03-22 16:46:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,13 +111,18 @@ app.get('/', function (req, res) {
 function hash (input, salt) {
     
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return hashed.toString();
 }
 
 
 app.get('/hash/:input', function(req, res){
     hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
+});
+
+app.get('/create-user', function(req, res){
+    
+    
 });
 
 var pool = new Pool(config)

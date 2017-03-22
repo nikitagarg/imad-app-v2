6af6bda7cf5ce166ac722d906d684e3beaf48a1c
nikitagarg <niki12garg@gmail.com>
2017-03-22 17:12:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var config = {
     user: 'nikitagarg',
     database: 'nikitagarg',
@@ -14,7 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.JSON());
 var articles = {
     'article-one': {
         title: 'Article One | Nikita Garg',
@@ -121,6 +122,9 @@ app.get('/hash/:input', function(req, res){
 });
 
 app.get('/create-user', function(req, res){
+    
+    var username = req.body.username;
+    var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
     pool.query("INSERT INTO 'user' (usrname, password) VALUES ($1, $2)", [username, dbString], function(err, result){
